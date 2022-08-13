bf3aa2d397e453db3f634dd72046d2280011484b
bkosm <bartoshmen@gmail.com>
2022-08-13 16:39:42 +0200

    fix: lower threshold for regression test

--- test/regression.ts
+++ test/regression.ts
@@ -1,6 +1,6 @@
 import { dirname, fromFileUrl } from "https://deno.land/std@0.57.0/path/mod.ts";
 import { decode, encode } from "https://deno.land/x/pngs@0.1.1/mod.ts";
-import { diff } from "https://cdn.skypack.dev/pixel-buffer-diff";
+import { diff } from "https://cdn.skypack.dev/-/pixel-buffer-diff@v1.3.2-y6RmJylRfpwHIB6SMZMr/dist=es2019,mode=imports/optimized/pixel-buffer-diff.js";
 import { AssertionError } from "https://deno.land/std@0.150.0/testing/asserts.ts";
 import { Page } from "https://deno.land/x/puppeteer@14.1.1/mod.ts";
 
@@ -29,7 +29,7 @@ const fileExists = async (filename: string): Promise<boolean> => {
 function performVisualRegression(
   baselineImagePath: string,
   candidateImagePath: string,
-  significanceThreshold = 0.01,
+  significanceThreshold = 0.05,
   cumulatedDiffThreshold = 138,
 ): RegressionResult {
   const pngBaseline = decode(Deno.readFileSync(baselineImagePath));
