06eb0fe23718ab372e28134c3cb8e01dd5f35529
bkosm <bartoshmen@gmail.com>
2022-08-13 16:55:52 +0200

    fix: even lower threshold for regression test

--- test/regression.ts
+++ test/regression.ts
@@ -29,8 +29,8 @@ const fileExists = async (filename: string): Promise<boolean> => {
 function performVisualRegression(
   baselineImagePath: string,
   candidateImagePath: string,
-  significanceThreshold = 0.15,
-  cumulatedDiffThreshold = 2000,
+  significanceThreshold = 0.2,
+  cumulatedDiffThreshold = 2100,
 ): RegressionResult {
   const pngBaseline = decode(Deno.readFileSync(baselineImagePath));
   const pngCandidate = decode(Deno.readFileSync(candidateImagePath));
