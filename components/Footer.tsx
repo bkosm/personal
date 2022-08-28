5a9566dda64d420f00052774d7051d94c084432b
bkosm <bartoshmen@gmail.com>
2022-08-28 22:45:39 +0200

    feat: pipeline status in footer - fancy stuff

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,7 +7,14 @@ export function Footer() {
     <footer
       class={tw`absolute bottom-0 w-full h-30 lg:px-05 md:px-20 sm:px-16 px-10 bg-gray-900 text-center text-white font-base sm:text-base text-sm`}
     >
-      <div>Made with 💖 and 🍋</div>
+      <div>
+        Made with 💖 and 🍋,{" "}
+        <img
+          class={tw`inline`}
+          src="https://github.com/bkosm/personal/actions/workflows/pipeline.yml/badge.svg"
+        />
+      </div>
+
       <a
         target="_blank"
         href="https://github.com/bkosm"
