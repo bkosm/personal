0913ed24a65d6985673c8b117d096fe5ca349641
bkosm <bartoshmen@gmail.com>
2022-08-28 22:38:42 +0200

    fix: comment out web3 part, because the error block gitalk

--- islands/Donation.tsx
+++ islands/Donation.tsx
@@ -4,19 +4,19 @@ import { useEffect, useState } from "preact/hooks";
 import { useWeb3Provider } from "../utils/hooks.ts";
 
 export default function Donation() {
-  const provider = useWeb3Provider();
   const [res, setRes] = useState<any | undefined>(undefined);
+  // const provider = useWeb3Provider();
 
-  useEffect(() => {
-    (async function () {
-      if (provider) {
-        const res = await provider.web3.eth.getBalance(
-          provider.mainAccountAddress,
-        );
-        setRes(res);
-      }
-    })();
-  }, [provider]);
+  // useEffect(() => {
+  //   (async function () {
+  //     if (provider) {
+  //       const res = await provider.web3.eth.getBalance(
+  //         provider.mainAccountAddress,
+  //       );
+  //       setRes(res);
+  //     }
+  //   })();
+  // }, [provider]);
 
   return (
     <Fragment>
