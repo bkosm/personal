63d9778f424a4c04eaa90ea1f56bfefefaad6af1
bkosm <bartoshmen@gmail.com>
2022-08-29 11:00:50 +0200

    feat: scale nav icon

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,13 +8,14 @@ interface Props extends h.JSX.HTMLAttributes<HTMLDivElement> {}
 export function Navbar(props: Props) {
   return (
     <div
-      {...props}
       class={tw`w-full lg:px-05 md:px-20 sm:px-16 p-3 bg-gray-900 mb-5 text-center text-white font-base sm:text-base text-sm`}
+      {...props}
     >
       <a href="/">
         <img
           src="/logo.svg"
           alt="the fresh logo: a sliced lemon dripping with juice"
+          width='50vw'
           class={tw`mx-auto`}
         />
       </a>
